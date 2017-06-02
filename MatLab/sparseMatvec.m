649ee248f4053dcc196a33c01daa26f952f40925
Neil Lindquist <archer1mail@gmail.com>
2017-06-02 12:55:37 -0500

    Make minor improvements to sparse matrix-vector mutliply
    Remove a pointless variable assignment. Also skip casting the values of the matrix to single precision at the begining, since they just get recast before use and everything needs to be transfered around to cast it anyways.

--- MatLab/sparseMatvec.m
+++ MatLab/sparseMatvec.m
@@ -2,12 +2,9 @@ function [b] = sparseMatvec(A, x)
 %mutliplies a sparse matrix by a vector
 
 [rows, cols, vals] = find(A);
-%as close to a single-precision sparse matrix as Matlab can do
-vals = single(vals);
 
 b = zeros(size(x));
 
-i=1;
 for i = 1:nnz(A)
-    b(rows(i)) = b(rows(i)) + double(vals(i))*double(x(cols(i)));
+    b(rows(i)) = b(rows(i)) + vals(i)*double(x(cols(i)));
 end
