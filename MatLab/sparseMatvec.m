57de0bf9934c051600147cca5ad6641e58699ab6
Neil Lindquist <archer1mail@gmail.com>
2017-06-02 09:06:15 -0500

    Fix sparse Matrix-Vector multiplication to use mixed precision

--- MatLab/sparseMatvec.m
+++ MatLab/sparseMatvec.m
@@ -1,16 +1,13 @@
 function [b] = sparseMatvec(A, x)
 %mutliplies a sparse matrix by a vector
 
-[m, n] = size(A);
-b = zeros(n, 1);
-
 [rows, cols, vals] = find(A);
+%as close to a single-precision sparse matrix as Matlab can do
+vals = single(vals);
 
-b = zeros(n, 1);
+b = zeros(size(x));
 
 i=1;
 for i = 1:nnz(A)
-    
-    b(rows(i)) = b(rows(i)) + vals(i)*x(cols(i));
-    
+    b(rows(i)) = b(rows(i)) + double(vals(i))*double(x(cols(i)));
 end
