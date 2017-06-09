0867eb406f27bf13dae1c4815404b8aca95ac7df
Neil Lindquist <archer1mail@gmail.com>
2017-06-09 13:45:25 -0500

    Create spcg error test for UF matrix 1850

--- MatLab/spcgErrorTest.m
+++ MatLab/spcgErrorTest.m
@@ -1,31 +1,29 @@
-function [errRatio, avgErr] = spcgErrorTest(iter, sz)
-    
-    if nargin < 1
-        iter = 10;
-    end
-    if nargin < 2
-        sz = 100;
-    end
-    
-    errRatio = 0;
-    avgErr = 0;
 
-    for i = 1:iter
-        A = sprandsym(sz, 3/sz, .1, 1);
-        x = rand(sz, 1);
-        b = A*x;
-        
-        As = sparseSingle(A);
-        bs = single(b);
-        
-        xm = spcg(As, bs);
-        xd = spcg(A, b);
-        
-        err = norm(double(xm)-x);
-        errRatio = errRatio + log2(err/norm(xd-x));
-        avgErr = avgErr + err;
+index = UFget();
 
-    end
+prob = UFget(1850, index);
+disp([prob.name ' - ' num2str(prob.id)]);
+
+problemCount = size(prob.b, 2);
+
+%40543 b's
+sA = sparseSingle(prob.A);
+L1 = ichol(prob.A, struct('type', 'ict', 'droptol', 1e-3, 'diagcomp', .05));
+L1T = L1';
+
+doubleErr = zeros(problemCount, 1);
+singleErr = zeros(problemCount, 1);
+disp('setup complete');
+for i = 1:problemCount
+    b = prob.b(:, 1);
+    sb = single(full(b));
     
-    errRatio = 2^(errRatio/iter);
-    avgErr = avgErr/iter;
+    [x, f] = pcg(prob.A, b, 1e-6, 100, L1, L1T);
+    sx = spcg(sA, sb, 1e-06, 100, L1, L1T);
+    
+    doubleErr(i) = norm(b-prob.A*x);
+    singleErr(i) = norm(b-prob.A*double(sx));
+    
+    disp(i);
+    save('pcg results - 1850.m', 'doubleErr', 'singleErr', 'i')
+end
