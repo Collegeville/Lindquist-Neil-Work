7ac58307f607cd77bd9b0aa59bb86f62db419d8b
Neil Lindquist <archer1mail@gmail.com>
2017-06-13 14:20:45 -0500

    Create plotErrors to show spcgErrorTest results

---
+++ MatLab/plotErrors.m
@@ -0,0 +1,10 @@
+
+[sErr, dErr, nB] = getErrors;
+
+hold off
+semilogy(sErr./nB)
+hold on
+semilogy(dErr./nB)
+semilogy([1,length(dErr)],[1e-6 1e-6], 'k-')
+semilogy([1,length(dErr)],[1e-5 1e-5], 'k-')
+semilogy([10,10],[1e-4 1e-7], 'k-')
