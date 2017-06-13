c00cf3d7e1b605d283bf0527f4b32b731c6c7bdd
Neil Lindquist <archer1mail@gmail.com>
2017-06-13 15:41:56 -0500

    Adjust error plot seperator line

--- MatLab/plotErrors.m
+++ MatLab/plotErrors.m
@@ -7,4 +7,4 @@ hold on
 semilogy(dErr./nB)
 semilogy([1,length(dErr)],[1e-6 1e-6], 'k-')
 semilogy([1,length(dErr)],[1e-5 1e-5], 'k-')
-semilogy([10,10],[1e-4 1e-7], 'k-')
+semilogy([10.5,10.5],[1e-4 1e-7], 'k-')
