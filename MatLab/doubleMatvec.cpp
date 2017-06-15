f0203d38bdf3485c547399d32b5472ae27988bf4
Neil Lindquist <archer1mail@gmail.com>
2017-06-15 17:00:51 -0500

    Create double version of spcg

---
+++ MatLab/doubleMatvec.cpp
@@ -0,0 +1,61 @@
+/*
+ * Computes the product of a the transpose of a sparse matrix and dense vector
+ * in double precision.
+ * 
+ * b = singleMatvec(AT, x)
+ */
+
+
+#include <cstdint>
+#include "matrix.h"
+#include "mex.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void mexFunction(int nlhs, mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[]) {
+    
+	const mxArray *A = prhs[0];
+    
+    mwIndex *Ac = mxGetIr(A);
+    mwIndex *Ar = mxGetJc(A);
+    double  *Av = mxGetPr(A);
+	
+	const double *x  = (double*)mxGetData(prhs[1])-1;
+    
+    const size_t Am = mxGetM(A);
+    
+    mxArray *b = mxCreateUninitNumericMatrix(Am, 1, mxDOUBLE_CLASS, mxREAL);
+    plhs[0] = b;
+    double *rawB = mxGetPr(b);    
+    
+    double temp = 0;
+    //0-indexed row-index
+    mwIndex row = Am-1;
+    //1-indexed el-index
+    mwIndex nextEnd = Ar[row];
+    //1-indexed el-index
+    mwIndex i = Ar[row+1]-1;
+    while(i > 0){
+        if(i < nextEnd){
+            rawB[row] = temp;
+            --row;
+            nextEnd = Ar[row];
+            temp = 0;
+        }else{
+            --i;
+            temp += Av[i] * x[Ac[i]];
+        }
+    }
+    rawB[row] = temp;
+    while(row > 0){
+        rawB[--row] = 0;
+    }
+}
+
+
+#ifdef __cplusplus
+}
+#endif
