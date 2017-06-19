13197131165ea11e0b65d6f31b633aedfed62da5
Neil Lindquist <archer1mail@gmail.com>
2017-06-19 08:22:03 -0500

    Add labels to error plot

--- MatLab/plotErrors.m
+++ MatLab/plotErrors.m
@@ -1,10 +1,19 @@
 
 [sErr, dErr, nB] = getErrors;
 
-hold off
-semilogy(sErr./nB)
-hold on
+
+%reset the plot
+clf('reset')
+
+
 semilogy(dErr./nB)
+hold on
+semilogy(sErr./nB)
 semilogy([1,length(dErr)],[1e-6 1e-6], 'k-')
 semilogy([1,length(dErr)],[1e-5 1e-5], 'k-')
-semilogy([10.5,10.5],[1e-4 1e-7], 'k-')
+%semilogy([10.5,10.5],[1e-4 1e-7], 'k-')
+
+
+ylabel('Relative Error of Result')
+xlabel('Problem Number')
+legend('Double Precision Error', 'Mixed Precision Error')
