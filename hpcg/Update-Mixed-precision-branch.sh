d51df6bd07f13c6491248f5e44582b8e550f246c
Neil Lindquist <archer1mail@gmail.com>
2017-07-10 09:09:05 -0500

    Make build and git update scripts abort on failure
    Prevents a failure on the double or mixed build from being hidden
    by the rest of the build
    
    Plus combine the git push's in update script to limit it to a
    single login failure

--- hpcg/Update-Mixed-precision-branch.sh
+++ hpcg/Update-Mixed-precision-branch.sh
@@ -6,7 +6,4 @@ git rebase --onto master mixed-precision-split
 git branch -f mixed-precision-split master
 
 #mixed-precision requires a forced push
-git push origin +mixed-precision
-#mixed-precision-split usually shouldn't need a forced push, so only
-#do so if nessaccery
-git push origin mixed-precision-split
+git push origin +master +mixed-precision +mixed-precision-split
