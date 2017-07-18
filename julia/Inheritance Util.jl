fdf0e9ea99fd8f3889f50dbc9616652ada81335f
Neil Lindquist <archer1mail@gmail.com>
2017-07-18 14:43:21 -0500

    Make docs in "Inheritance Util.jl" more Julian

--- julia/Inheritance Util.jl
+++ julia/Inheritance Util.jl
@@ -2,18 +2,18 @@
 #https://github.com/abeschneider/AbstractFields.jl/blob/master/src/AbstractFields.jl
 
 
-"""
+doc"""
     create_function_generator(name, functions)
 
 Creates a macro to generate instances of the given functions.
 The resulting macro takes a single type, which can be accessed
-with $typ.
+with `$typ`.
 
 This macro can be used to simulate multiple-inheritance, by using
 the resulting generator on each child type.
 
 # Examples
-`julia
+```jldoctest
 julia> @create_function_generator generate_combine begin
            function combine(a::$typ, b::$typ)
                return a+2*b
@@ -37,8 +37,7 @@ julia> combine([1, 2], [3, 4])
 
 julia> combine('a', 'b')
 ERROR: MethodError: no method matching combine(::Char, ::Char)
-`
-
+```
 """
 macro create_function_generator(name, functions)
     return esc(quote
