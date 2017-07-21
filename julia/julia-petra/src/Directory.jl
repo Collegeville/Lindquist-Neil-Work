ed6985901b47d6c9d6bbc3b02119e30f5949df57
Neil Lindquist <archer1mail@gmail.com>
2017-07-21 08:25:00 -0500

    Remove unnessacery size information

--- julia/julia-petra/src/Directory.jl
+++ julia/julia-petra/src/Directory.jl
@@ -3,7 +3,6 @@
 
 
 # DECISION: figure out types for global and local IDs
-# DECISION: figure out types for entry sizes
 """
 A base type as an interface to allow Map and BlockMap objects to reerence non-local
 elements.
@@ -12,12 +11,12 @@ All subtypes must have the following methods, with DirectoryImpl standing in for
 the subtype:
 
 getDirectoryEntries(directory::DirectoryImpl, Map::BlockMap, globalEntries::Array{GID},
-        high_rank_sharing_procs::Bool) where GID <: Integer
+        high_rank_sharing_procs::Bool)::Tuple{Array{PID}, Array{LID}}
+        where GID <: Integer where PID <: Integer where LID <:Integer
     - Returns processor and local id infor for non-local map entries.  Returns a tuple
         containing
             1 - an Array of processors owning the global ID's in question
             2 - an Array of local IDs of the global on the owning processor
-            3 - an Array containing the size of the objects associated with each global id
 
 gidsAllUniquelyOwned()
     - Returns true if all GIDs appear on just one processor
