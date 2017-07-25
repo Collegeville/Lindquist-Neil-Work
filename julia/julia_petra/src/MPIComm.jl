921a759adee64eb1bd40095fdf4e709c58e4fb24
Neil Lindquist <archer1mail@gmail.com>
2017-07-25 14:57:19 -0500

    Started creating MPI implementation of Comm

---
+++ julia/julia_petra/src/MPIComm.jl
@@ -0,0 +1,59 @@
+
+import MPI
+
+"""
+    MPIComm()
+An implementation of Comm using MPI
+The no argument constructor uses MPI.COMM_WORLD
+"""
+type MPIComm <: Comm
+    mpiComm::MPI.Comm
+end
+
+function MPIComm()
+    MPIComm(MPI.COMM_WORLD)
+end
+
+
+function barrier(comm::MPIComm)
+    MPI.Barrier(comm.mpiComm)
+end
+
+function broadcastAll(comm::MPIComm, myvals::Array{T}, root::Integer)::Array{T}
+    vals = copy(myvals)
+    
+    MPI.Bcast!(vals, root, comm.mpiComm)
+end
+
+function gatherAll(comm::MPIComm, myVals::Array{T})::Array{T} where T
+    MPI.Allgather(myVals, comm)
+end
+
+function sumAll(comm::MPIComm, partialsums::Array{T})::Array{T} where T
+    MPI.allreduce(partialsums, +, comm.mpiComm)
+end
+
+function maxAll(comm::MPIComm, partialmaxes::Array{T})::Array{T} where T
+    MPI.allreduce(partialsums, max, comm.mpiComm)
+end
+
+function minAll(comm::MPIComm, partialmins::Array{T})::Array{T} where T
+    MPI.allreduce(partialsums, min, comm.mpiComm)
+end
+
+function scanSum(comm::MPIComm, myvals::Array{T})::Array{T} where T
+    MPI.scan(myvals, length(myvals), +, comm.mpiComm)
+end
+
+function myPid(comm::MPIComm)::Integer
+    MPI.Comm_rank(comm.mpiComm) + 1
+end
+
+function numProc(comm::MPIComm)::Integer
+    MPI.Comm_size(comm.mpiComm)
+end
+
+createDistributor(comm::MPIComm)::Distributor - Create a distributor object
+
+createDirectory(comm::MPIComm, map::BlockMap)::Directory
+    - Create a directory object for the given Map
