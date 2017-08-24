2671de94162b9af0296a4194906fb4d379560965
neil-lindquist <archer1mail@gmail.com>
2017-08-24 14:24:10 -0500

    Create base of power-method
    Still fixing problems though

---
+++ julia/power-method/power-method.jl
@@ -0,0 +1,149 @@
+using julia_petra
+
+#program settings
+const useMPI = false
+
+const commData = Float64
+const commGID = UInt64
+const commPID = UInt8
+const commLID = UInt32
+
+
+function powerMethod(A::RowMatrix{Data, GID, PID, LID}, niters::Integer, tolerance::Number, verbose::Bool)::Data where {Data, GID, PID, LID}
+    powerMethod(A, niters, Data(tolerance), verbose)
+end
+
+"""
+Returns a tuple of the computed λ and if the λ is within tolerance
+"""
+function powerMethod(A::RowMatrix{Data, GID, PID, LID}, niters::Integer, tolerance::Data, verbose::Bool)::Tuple{Data, Bool} where {Data, GID, PID, LID}
+    q = MultiVector(getRowMap(A), 1)
+    z = MultiVector(getRowMap(A), 1)
+    reside = MultiVector(getRowMap(A), 1)
+
+    #skipping flop counting
+
+    randn(z.data)
+
+    valid = false
+
+    for iter = 1:niters
+        normz = norm2(z)
+        q = scale(z, 1.0/normz)
+        apply!(z, A, q, Data(1), Data(0))
+        λ = dot(q, z)
+        if iter%100 || iter+1 == niters
+            #TODO improve - currently works, but is a little bit of a hack around MultiVector's lack of math operators
+            @. resid.data = residual - λ*q.data
+            residual = norm2(resid)
+
+            verbose && print("Iter = $iter, λ = $λ, residual of A*q-λ*q = $residual\n")
+            if residual < tolerance
+                return (λ, true)
+            end
+        end
+    end
+    (λ, false)
+end
+
+macro log(values...)
+    :(verbose && println($(values...)))
+end
+
+function main(comm::Comm{GID, PID, LID}, arg1, numGlobalElements, verbose, Data::Type) where{GID, PID, LID}
+    const pid = myPid(comm)
+    const nProc = numProc(comm)
+
+    map = BlockMap(numGlobalElements, comm)
+
+    numMyElements = julia_petra.numMyElements(map)
+    myGlobalElements = julia_petra.myGlobalElements(map)
+    numNz = Array{GID, 1}(numMyElements)
+    for i = 1:numMyElements
+        if myGlobalElements[i] == 0 || myGlobalElements[i] == numGlobalElements
+            numNz[i] = 2
+        else
+            numNz[i] = 3
+        end
+    end
+
+    A = CSRMatrix{Data}(map, numNz)
+
+    values = Data[-1, -1]
+    indices = Array{GID, 1}(2)#TODO does this need to be GID?
+    two = Data(2)
+
+    for i = 1:numMyElements
+        if myGlobalElements[i] == 0
+            indices = [1]
+        elseif myGlobalElements[i] == numGlobalElements
+            indices = [numGlobalElements-1]
+        else
+            indices = [myGlobalElements[i], myGlobalElements[i]+2]
+        end
+
+        insertGlobalValues(myGlobalElements[i], values, indices)
+        insertGlobalValues(myGlobalElements[i], [two], [myGlobalElements[i]])
+    end
+
+    fillComplete(A)
+
+    const niters = numGlobalElements*10
+    const tolerance = 1.0e-2
+
+    tic()
+    λ, success = powerMethod(A, niters, tolerance, verbose)
+    #TODO look into storing FLOPS
+    elapsedTime = toc()
+
+    @log "λ = $λ; is within tolerance? $success"
+    @log "\n\n total time for first solve = $elapsedTime ms\n\n"
+    @log "increasing magnitude of first diagonal term, solving again\n\n"
+
+    if myGID(A, 1)
+        rowInds, rowVals = getGlobalRowView(A, 0)
+        for i = 1:numGlobalEntries(A, 0)
+            if rowInds[i] == 0
+                #using a view, so values update the original
+                rowVals[i] *= 10
+            end
+        end
+    end
+
+    tic()
+    λ, success = powerMethod(A, niters, tolerance, verbose)
+    elapsedTime = toc()
+
+    @log ""
+
+    @log "λ = $λ; is within tolerance? $success"
+    @log "\n\n total time for second solve = $elapsedTime ms\n\n"
+end
+
+
+
+if useMPI
+    comm = MPIComm(commGID, commPID, commLID)
+else
+    comm = SerialComm{commGID, commPID, commLID}()
+end
+
+const pid = myPid(comm)
+const nProc = numProc(comm)
+const verbose = pid == 1
+
+@log comm
+
+if length(ARGS) != 2
+    #@log "Usage: $(ARGS[1]) number_of_equations"
+    @log "Exactly 2 arguments are required"
+    exit(1)
+end
+
+numGlobalElements = parse(commLID, ARGS[2])
+if numGlobalElements < nProc
+    @log "numGlobalBlocks = $numGlobalElements cannot be < number of processors = $nProc"
+    exit(1)
+end
+
+main(comm, parse(commLID, ARGS[1]), numGlobalElements, verbose, commData)
