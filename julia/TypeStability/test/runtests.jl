6d28eb646ef0ecfd2ab1d95a6bf1a296f0248de0
Neil Lindquist <archer1mail@gmail.com>
2018-08-08 08:05:16 -0500

    Refactor TypeStability into a seperate Repo

--- julia/TypeStability/test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using TypeStability
-using Base.Test
-
-@testset "TypeStability.jl" begin
-
-    include("StabilityAnalysisTests.jl")
-    include("InlineCheckerTests.jl")
-    include("Utils.jl")
-end
